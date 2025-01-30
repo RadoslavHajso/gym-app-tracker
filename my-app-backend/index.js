e53db97763d52e21c29bd51dcd7eedad28ee65aa
Radoslav Hajšo <radoslavhajso@gmail.com>
2025-01-30 17:35:03 +0100

    Add PostgreSQL connection and test route

--- my-app-backend/index.js
+++ my-app-backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config(); // .env
+const pool = require("./config/db"); // Importujeme konfiguráciu DB
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-//test for starting server
+// Test pre pripojenie k DB
 app.get("/", (req, res) => {
     res.send("Hello, world! Server is running.");
+});
+
+// Príklad query na testovanie pripojenia k DB
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.send(result.rows[0]);
+    } catch (error) {
+        res.status(500).send("Error connecting to the database");
+    }
 });
 
 app.listen(port, () => {
